7988c932a23654619a899d6f335cc8ddc89c0176
halidaee <halidaee@gmail.com>
2024-08-27 18:39:54 -0400

    Generalized for non-Canon usage

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,11 @@
 package_list <- c("caret", "Rcpp", "future", "furrr", "rdist", "devtools")
-install.packages(package_list, dependencies = TRUE)
-devtools::install_github("mpleung/ARD/R/nuclear_ard")
+
+# Install packages not yet installed
+installed_packages <- package_list %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+    install.packages(package_list[!installed_packages])
+}
+
+if (!"nuclearARD" %in% installed.packages()) {
+    devtools::install_github("mpleung/ARD/R/nuclear_ard")
+}
